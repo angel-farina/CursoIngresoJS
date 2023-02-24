73b96d84bd5598bf13b08cdece9281faf79e4f37
angel-farina <zzyzxxrd@gmail.com>
2023-02-24 15:55:57 -0300

    updated

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -2,11 +2,11 @@
 
 // MODELO PARCIAL
 function mostrar() {
-    var titular;
-    var lugar;
+    var nombreTitular;
+    var lugarViaje;
     var temporada;
-    var dias;
-    var importe;
+    var diasCant;
+    var importeViaje;
     var altura;
     var peso;
     var sexo;
@@ -22,8 +22,23 @@ function mostrar() {
 	var importeGessel;
 	var importeMadryn;
 	var importeCordoba;
+	var costoGessel;
+	var costoMadryn;
+	var costoCordoba;
+	var maxImporte;
 	var cantidadTemporadaAlta;
 	var cantidadTemporadaBaja;
+	var diasGessel;
+	var diasMadryn;
+	var diasCordoba;
+	var diasMax;
+	var lugarMasDias;
+	var pesoMax;
+	var pesoMin;
+	var pesoTrue;
+	var nombrePesado;
+	var nombreLiviano;
+	var nombreLugarMasImporte;
 
 	cantidadPersonas = 0;
 	pesoTotalGessel = 0;
@@ -31,23 +46,38 @@ function mostrar() {
 	madryn = 0;
 	cordoba = 0;
 	sumaImportes = 0;
-	importeGessel = 1000;
-	importeMadryn = 2000;
-	importeCordoba = 3000;
+	importeGessel = 0;
+	importeMadryn = 0;
+	importeCordoba = 0;
+	costoGessel = 1000;
+	costoMadryn = 2000;
+	costoCordoba = 3000;
 	cantidadTemporadaAlta = 0;
 	cantidadTemporadaBaja = 0;
+	diasGessel = 0;
+	diasMadryn = 0;
+	diasCordoba = 0;
+	diasMax = 0;
+	lugarMasDias = "";
+	pesoMax = 0;
+	pesoMin = 0;
+	pesoTrue = true;
+	nombrePesado = "";
+	nombreLiviano = "";
+	maxImporte = 0;
+	nombreLugarMasImporte = "";
 
     continuar = true;
 
     while (continuar) {
-        titular = prompt('Ingrese nombre del titular');
-        while (titular == "") {
-            titular = prompt('ERROR, ingrese nombre del titular');
+        nombreTitular = prompt('Ingrese nombre del nombreTitular');
+        while (nombreTitular == "") {
+            nombreTitular = prompt('ERROR, ingrese nombre del nombreTitular');
         }
-        lugar = prompt('Ingrese su destino: Puerto Madryn, Villa Gessel o Cordoba');
-		lugar = lugar.toLowerCase();
-		while (lugar != "puerto madryn" && lugar != "villa gessel" && lugar != "cordoba" || lugar == "") {
-			lugar = prompt('ERROR, ingrese su destino: Puerto Madryn, Villa Gessel o Cordoba');
+        lugarViaje = prompt('Ingrese su destino: Puerto Madryn, Villa Gessel o Cordoba');
+		lugarViaje = lugarViaje.toLowerCase();
+		while (lugarViaje != "puerto madryn" && lugarViaje != "villa gessel" && lugarViaje != "cordoba" || lugarViaje == "") {
+			lugarViaje = prompt('ERROR, ingrese su destino: Puerto Madryn, Villa Gessel o Cordoba');
 		}
 
 		temporada = prompt('Ingrese temporada: Alta o Baja');
@@ -56,9 +86,9 @@ function mostrar() {
 			temporada = prompt('ERROR, ingrese temporada: Alta o Baja');
 		}
 
-		dias = parseInt(prompt('Ingrese cantidad de dias'));
-		while (isNaN(dias) || dias < 0) {
-			dias = parseInt(prompt('ERROR, ingrese cantidad de dias'));
+		diasCant = parseInt(prompt('Ingrese cantidad de diasCant'));
+		while (isNaN(diasCant) || diasCant < 0) {
+			diasCant = parseInt(prompt('ERROR, ingrese cantidad de diasCant'));
 		}
 
 		altura = parseFloat(prompt('Ingrese su altura'));
@@ -68,7 +98,7 @@ function mostrar() {
 
 		peso = parseFloat(prompt('Ingrese su peso'));
 		while (isNaN(peso)) {
-			peso = parseFloat(prompt('ERROR, ingrese su peso'));		
+			peso = parseFloat(prompt('ERROR, ingrese su peso'));
 		}
 
 		sexo = prompt('Ingrese su sexo: F, M o NB');
@@ -90,9 +120,73 @@ function mostrar() {
 
 		cantidadPersonas++;
 
-		if (lugar == "villa gessel") {
-			gessel++;
-			pesoTotalGessel+=peso;
+		switch (lugarViaje) {
+			case "villa gessel":
+				gessel++;
+				pesoTotalGessel+=peso;	
+				diasGessel += diasCant;	
+				importeGessel += costoGessel;
+				break;
+			case "puerto madryn":
+				diasMadryn += diasCant;
+				importeMadryn += costoMadryn;
+				break;
+			case "cordoba":
+				diasCordoba += diasCant;
+				importeCordoba += costoCordoba;
+				break;
+		}
+
+		if (diasGessel > diasMax) {
+			diasMax = diasGessel;
+			lugarMasDias = "Villa Gessel";
+		}
+		
+		if (diasMadryn > diasMax) {
+			diasMax = diasMadryn;
+			lugarMasDias = "Puerto Madryn";
+		}
+		
+		if (diasCordoba > diasMax) {
+			diasMax = diasCordoba;
+			lugarMasDias = "Cordoba";
+		}
+
+		if (temporada == "alta") {
+			cantidadTemporadaAlta++;
+		} else {
+			cantidadTemporadaBaja++;
+		}
+
+		if (pesoTrue == true) {
+			pesoTrue = false
+			pesoMax = peso;
+			pesoMin = peso;
+		} else {
+			if (peso > pesoMax) {
+				pesoMax = peso;
+				nombrePesado = nombreTitular
+			} else {
+				if (peso < pesoMin) {
+					pesoMin = peso;
+					nombreLiviano =  nombreTitular;
+				}
+			}
+		}
+
+		if (importeGessel > maxImporte) {
+			maxImporte = importeGessel;
+			nombreLugarMasImporte = "Villa Gessel";
+		}
+
+		if (importeMadryn > maxImporte) {
+			maxImporte = importeMadryn;
+			nombreLugarMasImporte = "Puerto Madryn";
+		}
+		
+		if (importeCordoba > maxImporte) {
+			maxImporte = importeCordoba;
+			nombreLugarMasImporte = "Cordoba";
 		}
 
 		if (temporada == "alta") {
@@ -102,10 +196,10 @@ function mostrar() {
 		}
 
 		console.log(`
-		titular: ${titular}
-		lugar: ${lugar}
+		nombreTitular: ${nombreTitular}
+		lugarViaje: ${lugarViaje}
 		temporada: ${temporada}
-		dias: ${dias}
+		diasCant: ${diasCant}
 		altura: ${altura}
 		peso: ${peso}
 		sexo: ${sexo}
@@ -120,7 +214,28 @@ function mostrar() {
         continuar = confirm('Quiere ingresar otro Pasajero?');
     }
 
-	
+	sumaImportes = importeGessel + importeMadryn + importeCordoba;
+
+	console.log(`
+	NombreTitular: ${nombreTitular}
+	LugarViaje: ${lugarViaje}
+	Temporada: ${temporada}
+	DiasCant: ${diasCant}
+	Altura: ${altura}
+	Peso: ${peso}
+	Sexo: ${sexo}
+	Equipaje: ${equipaje}
+	Pago: ${pago}
+	Cantidad de personas: ${cantidadPersonas}
+	Cantidad a gessel: ${gessel}
+	Cantidad personas temporada alta: ${cantidadTemporadaAlta}
+	Cantidad personas temporada baja: ${cantidadTemporadaBaja}
+	Peso de personas total gessel: ${pesoTotalGessel}
+	Suma Importes: ${sumaImportes}
+	Lugar con mas dias acumulados: ${lugarMasDias}
+	Nombre persona mas pesada: ${nombrePesado}
+	NOmbre persona mas liviana: ${nombreLiviano}
+	`)
 }
 
 // BLACKJACK
